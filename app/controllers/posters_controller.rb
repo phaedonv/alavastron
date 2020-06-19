359cd1f7073db8a73c099c16edeb467ccaee4aa7
Phaedon Valasiadis <faival@protonmail.com>
2020-06-19 14:11:51 +0300

    added some controller stuff..

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -1,24 +1,40 @@
 class PostersController < ApplicationController
 
   def index
+    @posters = Poster.all
   end
 
   def show
   end
 
   def new
+    @poster = Poster.new
   end
 
   def create
+    @poster = Poster.new(poster_params)
+
+    if @poster.save
+      redirect_to @poster
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @poster.update(poster_params)
+    if @poster.save
+      redirect_to @poster
+    else
+      render :new
+    end
   end
 
   def destroy
+    @poster.destroy
   end
 
   private
